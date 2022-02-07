d3fe2d9c5a8fc6cd4dd32011d1e2e4c4d45e3d8a
azahid6ix <80329619+azahid6ix@users.noreply.github.com>
2022-02-07 23:45:38 +0500

    fix saml issue (#8)

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -2,7 +2,7 @@ import { GetServerSidePropsContext } from "next";
 import { getCsrfToken, signIn } from "next-auth/react";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 
 import { ErrorCode, getSession } from "@lib/auth";
 // import { WEBSITE_URL } from "@lib/config/constants";
@@ -32,6 +32,8 @@ export default function Login({
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [code, setCode] = useState("");
+  const [loader, setLoader] = useState(true);
+  const samlRef = useRef<any>();
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [secondFactorRequired, setSecondFactorRequired] = useState(false);
   const [errorMessage, setErrorMessage] = useState<string | null>(null);
@@ -45,6 +47,11 @@ export default function Login({
   };
 
   const callbackUrl = typeof router.query?.callbackUrl === "string" ? router.query.callbackUrl : "/";
+
+  useEffect(() => {
+    samlRef?.current?.click();
+    setLoader(false);
+  }, []);
 
   async function handleSubmit(e: React.SyntheticEvent) {
     e.preventDefault();
@@ -97,150 +104,158 @@ export default function Login({
   });
 
   return (
-    <div className="flex flex-col justify-center min-h-screen py-12 bg-neutral-50 sm:px-6 lg:px-8">
-      <HeadSeo title={t("login")} description={t("login")} />
-
-      {isSubmitting && (
-        <div className="absolute z-50 flex items-center w-full h-screen bg-gray-50">
-          <Loader />
+    <>
+      {loader && (
+        <div className="page-loader">
+          <div className="holder"></div>
         </div>
       )}
+      <div className="flex flex-col justify-center min-h-screen py-12 bg-neutral-50 sm:px-6 lg:px-8">
+        <HeadSeo title={t("login")} description={t("login")} />
 
-      <div className="sm:mx-auto sm:w-full sm:max-w-md">
-        <img className="h-6 mx-auto" src="/calendso-logo-white-word.svg" alt="Cal.com Logo" />
-        <h2 className="mt-6 text-3xl font-bold text-center font-cal text-neutral-900">
-          {t("sign_in_account")}
-        </h2>
-      </div>
+        {isSubmitting && (
+          <div className="absolute z-50 flex items-center w-full h-screen bg-gray-50">
+            <Loader />
+          </div>
+        )}
 
-      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-        <div className="px-4 py-8 mx-2 bg-white border rounded-sm sm:px-10 border-neutral-200">
-          <form className="space-y-6" onSubmit={handleSubmit}>
-            <input name="csrfToken" type="hidden" defaultValue={csrfToken || undefined} hidden />
-            <div>
-              <label htmlFor="email" className="block text-sm font-medium text-neutral-700">
-                {t("email_address")}
-              </label>
-              <div className="mt-1">
-                <EmailInput
-                  id="email"
-                  name="email"
-                  required
-                  value={email}
-                  onInput={(e) => setEmail(e.currentTarget.value)}
-                  className="block w-full px-3 py-2 placeholder-gray-400 border rounded-sm shadow-sm appearance-none border-neutral-300 focus:outline-none focus:ring-neutral-900 focus:border-neutral-900 sm:text-sm"
-                />
-              </div>
-            </div>
+        <div className="sm:mx-auto sm:w-full sm:max-w-md">
+          <img className="h-6 mx-auto" src="/calendso-logo-white-word.svg" alt="Cal.com Logo" />
+          <h2 className="mt-6 text-3xl font-bold text-center font-cal text-neutral-900">
+            {t("sign_in_account")}
+          </h2>
+        </div>
 
-            <div>
-              <div className="flex">
-                <div className="w-1/2">
-                  <label htmlFor="password" className="block text-sm font-medium text-neutral-700">
-                    {t("password")}
-                  </label>
-                </div>
-                <div className="w-1/2 text-right">
-                  <Link href="/auth/forgot-password">
-                    <a tabIndex={-1} className="text-sm font-medium text-primary-600">
-                      {t("forgot")}
-                    </a>
-                  </Link>
-                </div>
-              </div>
-              <div className="mt-1">
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  autoComplete="current-password"
-                  required
-                  value={password}
-                  onInput={(e) => setPassword(e.currentTarget.value)}
-                  className="block w-full px-3 py-2 placeholder-gray-400 border rounded-sm shadow-sm appearance-none border-neutral-300 focus:outline-none focus:ring-neutral-900 focus:border-neutral-900 sm:text-sm"
-                />
-              </div>
-            </div>
-
-            {secondFactorRequired && (
+        <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+          <div className="px-4 py-8 mx-2 bg-white border rounded-sm sm:px-10 border-neutral-200">
+            <form className="space-y-6" onSubmit={handleSubmit}>
+              <input name="csrfToken" type="hidden" defaultValue={csrfToken || undefined} hidden />
               <div>
                 <label htmlFor="email" className="block text-sm font-medium text-neutral-700">
-                  {t("2fa_code")}
+                  {t("email_address")}
                 </label>
                 <div className="mt-1">
-                  <input
-                    id="totpCode"
-                    name="totpCode"
-                    type="text"
-                    maxLength={6}
-                    minLength={6}
-                    inputMode="numeric"
-                    value={code}
-                    onInput={(e) => setCode(e.currentTarget.value)}
+                  <EmailInput
+                    id="email"
+                    name="email"
+                    required
+                    value={email}
+                    onInput={(e) => setEmail(e.currentTarget.value)}
                     className="block w-full px-3 py-2 placeholder-gray-400 border rounded-sm shadow-sm appearance-none border-neutral-300 focus:outline-none focus:ring-neutral-900 focus:border-neutral-900 sm:text-sm"
                   />
                 </div>
               </div>
+
+              <div>
+                <div className="flex">
+                  <div className="w-1/2">
+                    <label htmlFor="password" className="block text-sm font-medium text-neutral-700">
+                      {t("password")}
+                    </label>
+                  </div>
+                  <div className="w-1/2 text-right">
+                    <Link href="/auth/forgot-password">
+                      <a tabIndex={-1} className="text-sm font-medium text-primary-600">
+                        {t("forgot")}
+                      </a>
+                    </Link>
+                  </div>
+                </div>
+                <div className="mt-1">
+                  <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    autoComplete="current-password"
+                    required
+                    value={password}
+                    onInput={(e) => setPassword(e.currentTarget.value)}
+                    className="block w-full px-3 py-2 placeholder-gray-400 border rounded-sm shadow-sm appearance-none border-neutral-300 focus:outline-none focus:ring-neutral-900 focus:border-neutral-900 sm:text-sm"
+                  />
+                </div>
+              </div>
+
+              {secondFactorRequired && (
+                <div>
+                  <label htmlFor="email" className="block text-sm font-medium text-neutral-700">
+                    {t("2fa_code")}
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      id="totpCode"
+                      name="totpCode"
+                      type="text"
+                      maxLength={6}
+                      minLength={6}
+                      inputMode="numeric"
+                      value={code}
+                      onInput={(e) => setCode(e.currentTarget.value)}
+                      className="block w-full px-3 py-2 placeholder-gray-400 border rounded-sm shadow-sm appearance-none border-neutral-300 focus:outline-none focus:ring-neutral-900 focus:border-neutral-900 sm:text-sm"
+                    />
+                  </div>
+                </div>
+              )}
+
+              <div className="space-y-2">
+                <button
+                  type="submit"
+                  disabled={isSubmitting}
+                  className="flex justify-center w-full px-4 py-2 text-sm font-medium text-white border border-transparent rounded-sm shadow-sm bg-neutral-900 hover:bg-neutral-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-black">
+                  {t("sign_in")}
+                </button>
+              </div>
+
+              {errorMessage && <p className="mt-1 text-sm text-red-700">{errorMessage}</p>}
+            </form>
+            {isGoogleLoginEnabled && (
+              <div style={{ marginTop: "12px" }}>
+                <button
+                  data-testid={"google"}
+                  onClick={async () => await signIn("google")}
+                  className="flex justify-center w-full px-4 py-2 text-sm font-medium text-black border border-transparent rounded-sm shadow-sm bg-secondary-50 hover:bg-secondary-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-black">
+                  {t("signin_with_google")}
+                </button>
+              </div>
             )}
+            {isSAMLLoginEnabled && (
+              <div style={{ marginTop: "12px" }}>
+                <button
+                  data-testid={"saml"}
+                  ref={samlRef}
+                  onClick={async (event) => {
+                    event.preventDefault();
 
-            <div className="space-y-2">
-              <button
-                type="submit"
-                disabled={isSubmitting}
-                className="flex justify-center w-full px-4 py-2 text-sm font-medium text-white border border-transparent rounded-sm shadow-sm bg-neutral-900 hover:bg-neutral-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-black">
-                {t("sign_in")}
-              </button>
-            </div>
+                    if (!hostedCal) {
+                      await signIn("saml", {}, { tenant: samlTenantID, product: samlProductID });
+                    } else {
+                      if (email.length === 0) {
+                        setErrorMessage(t("saml_email_required"));
+                        return;
+                      }
 
-            {errorMessage && <p className="mt-1 text-sm text-red-700">{errorMessage}</p>}
-          </form>
-          {isGoogleLoginEnabled && (
-            <div style={{ marginTop: "12px" }}>
-              <button
-                data-testid={"google"}
-                onClick={async () => await signIn("google")}
-                className="flex justify-center w-full px-4 py-2 text-sm font-medium text-black border border-transparent rounded-sm shadow-sm bg-secondary-50 hover:bg-secondary-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-black">
-                {t("signin_with_google")}
-              </button>
-            </div>
-          )}
-          {isSAMLLoginEnabled && (
-            <div style={{ marginTop: "12px" }}>
-              <button
-                data-testid={"saml"}
-                onClick={async (event) => {
-                  event.preventDefault();
-
-                  if (!hostedCal) {
-                    await signIn("saml", {}, { tenant: samlTenantID, product: samlProductID });
-                  } else {
-                    if (email.length === 0) {
-                      setErrorMessage(t("saml_email_required"));
-                      return;
+                      // hosted solution, fetch tenant and product from the backend
+                      mutation.mutate({
+                        email,
+                      });
                     }
-
-                    // hosted solution, fetch tenant and product from the backend
-                    mutation.mutate({
-                      email,
-                    });
-                  }
-                }}
-                className="flex justify-center w-full px-4 py-2 text-sm font-medium text-black border border-transparent rounded-sm shadow-sm bg-secondary-50 hover:bg-secondary-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-black">
-                {t("signin_with_saml")}
-              </button>
-            </div>
-          )}
-        </div>
-        {/* <div className="mt-4 text-sm text-center text-neutral-600">
+                  }}
+                  className="flex justify-center w-full px-4 py-2 text-sm font-medium text-black border border-transparent rounded-sm shadow-sm bg-secondary-50 hover:bg-secondary-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-black">
+                  {t("signin_with_saml")}
+                </button>
+              </div>
+            )}
+          </div>
+          {/* <div className="mt-4 text-sm text-center text-neutral-600">
           {t("dont_have_an_account")} 
           <a href={`${WEBSITE_URL}/signup`} className="font-medium text-neutral-900">
             {t("create_an_account")}
           </a>
         </div> */}
-      </div>
+        </div>
 
-      <AddToHomescreen />
-    </div>
+        <AddToHomescreen />
+      </div>
+    </>
   );
 }
 
